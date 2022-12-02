fccbfb3af82d46bf4f48197ac93e3cc234bb15c6
Anatolit Novakovskiy <anatoliy.n25@gmail.com>
2022-12-02 19:56:45 +0200

    Complite desctop page

--- js/main.js
+++ js/main.js
@@ -29,6 +29,8 @@
 
 // }
 
+
+
 $(window).scroll(function () {
 
 });
@@ -42,8 +44,8 @@ $(document).ready(function () {
             modal_bg = $('.events_modal-bg');
         modal_bg.addClass('active').attr('data-id', target_id);
         setTimeout(function() { 
-            target_modal.fadeIn(300);
-        }, 300);
+            target_modal.fadeIn(100);
+        }, 100);
     });
 
     // Close modal
@@ -52,17 +54,38 @@ $(document).ready(function () {
         let modal_bg = $('.events_modal-bg'),
             target_id = modal_bg.attr('data-id'),
             target_modal = $('.events_modal[data-id="' + target_id + '"]');
-        target_modal.fadeOut(300);
+        target_modal.fadeOut(100);
         setTimeout(function() { 
             modal_bg.removeClass('active').attr('data-id', '');
-        }, 400);
+        }, 200);
     });
 
 
+
+
+
+    // $('ul>li>a').bind('click',function(event){
+    //     var $anchor = $(this);
+
+    //     $('html, body').stop().animate({
+    //         scrollLeft: $($anchor.attr('href')).offset().left
+    //     }, 1000);
+    //     event.preventDefault();
+    // });
+    
+    // $(document).on('wheel', '.events_content', function(e){
+    //     const race = 25;
+    //     let oe = e.originalEvent;
+    //     if (oe.deltaY > 0) {
+    //         $('this').scrollLeft += race;
+    //     } else {
+    //         $('this').scrollLeft -= race;
+    //     }
+    // });
     const mouseWheel = document.querySelector('.events_content');
 
     mouseWheel.addEventListener('wheel', function(e) {
-        const race = 40; // How many pixels to scroll
+        const race = 15; // How many pixels to scroll
 
         if (e.deltaY > 0) // Scroll right
             mouseWheel.scrollLeft += race;
